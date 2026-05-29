49c6deade92f0f9f45f51817253c87bef51ecfea
dev-miho <bojkovskimihail47@gmail.com>
2026-05-29 15:51:32 +0200

    Updated

---
+++ Mathematics 3/Exam problems/Parametric Tests/1./main.r
@@ -0,0 +1,22 @@
+library(BSDA)
+
+#H0:mu = 69
+#HA:mu != 69
+
+x<-c(68, 65, 68, 72, 68, 75, 70, 72, 70, 72, 65, 83, 72, 68, 72, 70, 65, 70, 70, 70, 75)
+
+sigma=4
+
+mu=69
+
+alpha=0.05
+
+result<-z.test(x,mu=mu,sigma.x=sigma,alternative='two.sided',conf.level=(1-alpha))
+
+result$statistic
+result$p.value
+
+result$p.value < alpha
+
+#Since the p-value is greater than alpha,we do not reject the null hypothesis.
+#Conclusion:There is not enough evidence to support the claim that average heart rate of this group differs from the previously reported average of 69 bpm.
